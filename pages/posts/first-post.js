e13f8d0028b77c9ac50965395c661e2755f225f5
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-13 03:36:12 +0200

    assets metadata and css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
